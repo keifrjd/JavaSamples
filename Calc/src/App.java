1da224a135092c29bb8580035883a3dfd6aa2441
Lushakov Sergey <lushakovsergei@yandex.ru>
2024-03-10 18:35:09 +0300

    class object cycles

--- Calc/src/App.java
+++ Calc/src/App.java
@@ -3,7 +3,38 @@ public class App {
      * @param args
      * @throws Exception
      */
+
+
     public static void main(String[] args) throws Exception {
+        class Point{
+            int x,y;
+            void print(Point this){
+                System.out.println("Point<"+this.x+","+ this.y + ">");
+            }
+            double norm(){
+                return Math.sqrt(x*x + y*y);
+            }
+            void shift(int dx, int dy){
+                x +=dx;
+                y +=dy;
+            }
+            @Override
+            public String toString(){
+                return "Point<"+this.x+","+ this.y + ">";
+            }
+        }
+        Point point = new Point();
+        point.x = 5;
+        point.y = 6;
+        Point point2 = new Point();
+        point2.x = 7;
+        point2.y = 8;
+        point = point2;
+        point.x = 9;
+        point.print( );
+        double point_norm = point.norm();
+        String str = point.toString();
+
         System.out.println("Hello, World!");
         int x = 999;
         int y = 33;
@@ -33,5 +64,44 @@ public class App {
        System.out.println(100 > 10 && 100 > 200);
        System.out.println(100 > 50 && 100 >= 100);
        System.out.println(100 > 10 || 100 > 200);
+
+       System.out.println(x>0 ? "positive" : "negative or zero");
+
+       // Unboxing
+       Double doubler = 1.0;
+       Integer integerr = 2;
+       Number n = false ? doubler : integerr;
+       System.out.println(n);//2.0
+
+
+       int integerSwitch = 1;
+       switch (integerSwitch) {
+        case 1 : System.out.println("one");
+            break;
+        case 3 : System.out.println("two or three"); 
+            break;
+        default : { 
+            System.out.println("other:");
+            System.out.println(integerSwitch);
+            break;
+        }
+       }
+       //infinity loop
+       //while (true) {
+       // System.out.println("catch me if you can!");
+       //}
+       int dou = 0;
+       do {
+        System.out.println(dou);
+        dou++;
+       } while(false);
+      // while(dou<10);
+      for(int p=0;p<10;p++){
+        System.out.println(p);
+      }
+      //for(;;){}//бесконечный цикл
+     // for(String arg: args){ цикл по массиву
+     //  System.out.println(arg);
+     // }
     }
 }
